ba5a3954ad5d0ffbd071e500ce3ca46bef5c80b8
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-17 18:55:59 +1100

    feat: project init

--- README.md
+++
@@ -1,2 +0,0 @@
-# Web-Music
-Netease Cloud Music
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import HooksDemo from './HooksDemo';
-
-ReactDOM.render(<HooksDemo />, document.getElementById('root'));
